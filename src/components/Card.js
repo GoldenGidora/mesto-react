1c787639e72a5c97a9342bc0aa24094bf82c5b36
Artem <artemjay@yandex.ru>
2023-07-16 18:49:33 +0300

    Refactor: ✨улучшения✨

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,20 +5,18 @@ const Card = ({card, onCardClick}) => {
         onCardClick(card);
     }
     return (
-        <div className="cards__item" key={card._id}>
+        <div className="cards__item">
             <button type="button" className="cards__delete"></button>
             <img className="cards__img"
                  src={card.link}
                  alt={card.name}
-                onClick={handleCardClick}
+                 onClick={handleCardClick}
             />
             <div className="cards__info">
                 <h2 className="cards__title">{card.name}</h2>
                 <div className="cards__likes">
                     <button type="button" className="cards__like"></button>
-                    <span className="cards__like-number">
-                                        {card.likes.length}
-                                    </span>
+                    <span className="cards__like-number"> {card.likes.length} </span>
                 </div>
             </div>
         </div>
