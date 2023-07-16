47966143783df981209d6ed8f460e7ce4b4cdf11
Artem <artemjay@yandex.ru>
2023-07-16 18:51:36 +0300

    Refactor: Деструктуризация пропсов

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-function PopupWithForm(props) {
+function PopupWithForm({name, title, isOpen, submitText, onClose, children}) {
 
     return (
-        <div className={`popup popup_type_${props.name} ${ props.isOpen ? "popup_opened" : ""}`}>
+        <div className={`popup popup_type_${name} ${ isOpen ? "popup_opened" : ""}`}>
             <div className="popup__container">
-                <h2 className="popup__title">{props.title}</h2>
+                <h2 className="popup__title">{title}</h2>
                 <form
                     className="form"
-                    name={`${props.name}`}
+                    name={`${name}`}
                     action="#"
                 >
-                    {props.children}
-                    <button type="submit" className="popup__submit">{props.submitText}</button>
+                    {children}
+                    <button type="submit" className="popup__submit">{submitText}</button>
                 </form>
-                <button type="button" className="popup__close" onClick={props.onClose}/>
+                <button type="button" className="popup__close" onClick={onClose}/>
             </div>
         </div>
     )
